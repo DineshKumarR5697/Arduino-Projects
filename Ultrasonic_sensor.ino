a540516351f0b41e11201fd088f48018159addcc
DineshKumar R <rdinesh5697@gmail.com>
2018-01-14 10:48:44 +0530

    Add files via upload
    This code is my base code for distance measurement

---
+++ Ultrasonic_sensor.ino
@@ -0,0 +1,24 @@
+#define echoPin 11
+#define trigPin 12
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  int distance,duration;
+  digitalWrite(trigPin,HIGH);
+  delay(100);
+  digitalWrite(trigPin,LOW);
+  duration=pulseIn(echoPin,HIGH);
+  distance=(duration/2)/29.1;
+  Serial.print(distance);
+  Serial.println("CM");
+  Serial.print("");
+  delay(1000);
+}
